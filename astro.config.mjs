5406ac04e93825182837160fbecc6b9e869d80ed
kavehrafie <kavehrafie@gmail.com>
2023-10-24 18:01:33 -0500

    - roll up

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/utils/config.ts';
 import compress from 'astro-compress';
 import vercel from "@astrojs/vercel/serverless";
+import commonjs from '@rollup/plugin-commonjs';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
@@ -28,6 +30,10 @@ export default defineConfig({
         '@': path.resolve(__dirname, './src')
       }
     }
+  },
+  rollup(config) {
+    config.plugins.push(commonjs());
+    return config;
   },
   output: "server",
   adapter: vercel()
